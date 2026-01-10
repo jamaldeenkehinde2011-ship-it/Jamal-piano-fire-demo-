a21a0508c93ba45ee36ad2af1125a8e26e848f85
jamaldeenkehinde2011-ship-it <jamaldeenkehinde2011@gmail.com>
2026-01-11 00:43:13 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GameUI from './components/GameUI';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🎹 Audio Piano Tiles Game</h1>
+      <GameUI />
+    </div>
+  );
+}
+
+export default App;
